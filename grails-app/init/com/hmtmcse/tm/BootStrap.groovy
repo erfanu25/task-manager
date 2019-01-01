c0883ea9bf930fa363f46912526ef119dbb251d8
Touhid Mia <hmtm.cse@gmail.com>
2019-01-01 22:22:35 +0600

    Todo List

--- grails-app/init/com/hmtmcse/tm/BootStrap.groovy
+++ grails-app/init/com/hmtmcse/tm/BootStrap.groovy
@@ -1,11 +1,19 @@
 package com.hmtmcse.tm
 
+import com.hmtmcse.websocket.ChatEndpoint
+
+import javax.websocket.server.ServerContainer
+
 class BootStrap {
 
     AppInitService appInitService
 
     def init = { servletContext ->
+
         appInitService.initUser()
+        ServerContainer serverContainer = servletContext.getAttribute("javax.websocket.server.ServerContainer")
+        serverContainer.addEndpoint(ChatEndpoint)
+
     }
     def destroy = {
     }
